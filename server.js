920dd11268dbcd57a92e9a1fdf6bfbccb67e5f2d
shankars99 <shankars0899@gmail.com>
2020-10-01 00:06:59 +0530

    Standard stuff

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var routes       = require("./routes.js");
 const app        = express();
 const port       = 4000;
 
-global.loc = "/home/shankar/Documents/projects/srp-user-verification/views";
+global.loc = "/home/shankar/git-repo/srp-user-verification/views";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 routes(app);
 
 app.listen(port, () => {
-  console.log(`Success! Your application is running on port ${port}.`);
+  console.log(`Application runnning on ${port}.`);
 });
