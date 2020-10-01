dde2c3a485a39534148fb578cf66e59fc41aacf1
shankars99 <shankars0899@gmail.com>
2020-10-01 13:58:21 +0530

    Website display - Success

--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
 (function(){
-
-
+    var assetsController = require('./controllers/assetsController');
     var homeController = require('./controllers/homeController');
     var signupController = require('./controllers/signupController');
-    /*
     var showQRController = require('./controllers/showQRController');
-*/
+
 	module.exports = function(app){
-        app.get("/home",homeController);
+
+        app.all("/home",homeController);
+//        app.get("/home/*",homeController);
         app.all("/signup",signupController);
-        /*
         app.get("/showQR",showQRController);
-*/
+        app.get("/*",assetsController);
+
   };
 })();
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express    = require('express');
+const morgan     = require('morgan');
 const bodyParser = require('body-parser');
 var routes       = require("./routes.js");
 const app        = express();
@@ -7,6 +8,11 @@ const port       = 4000;
 global.loc = "/home/shankar/git-repo/srp-user-verification/views";
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'))
+app.use(express.static('assets'))
+app.use(express.static('images'))
+
+//app.use(morgan('combined'))
 
 routes(app);
 
