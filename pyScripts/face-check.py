3c592a85fa2765b95fdf581c64ae4805b3618d60
shankars99 <shankars0899@gmail.com>
2021-01-07 21:54:38 +0530

    completed

--- pyScripts/face-check.py
+++ pyScripts/face-check.py
@@ -13,18 +13,21 @@ root_dir = "/home/shankar/git-repo/srp-user-verification/pyScripts/"
 model    = keras.models.load_model(root_dir +"saves")
 
 test_images = root_dir + "../models"
-img_num     = "images"
 
-img=cv2.imread(os.path.join(test_images,img_num+'.jpeg'))
+for j in range(6):
+    i = str(j+1)
 
-img=cv2.imread(os.path.join(test_images,img_num+'.jpeg'),cv2.IMREAD_GRAYSCALE)
-new_img=cv2.resize(img,(50,50))
-new_img=new_img.reshape(-1,50,50,1)
-#predict=model.predict(new_img)
-predict = np.argmax(model.predict(new_img) > 0.5).astype("int32")
-#print(predict)
+    img_num     = "test" + str(i)
+    img=cv2.imread(os.path.join(test_images,img_num+'.jpeg'))
 
-if predict == 0:
-    print("no mask")
-else:
-    print("wearing mask")
+    img=cv2.imread(os.path.join(test_images,img_num+'.jpeg'),cv2.IMREAD_GRAYSCALE)
+    new_img=cv2.resize(img,(50,50))
+    new_img=new_img.reshape(-1,50,50,1)
+    #predict=model.predict(new_img)
+    predict = np.argmax(model.predict(new_img) > 0.5).astype("int32")
+    #print(predict)
+
+    if predict == 0:
+        print("test"+ i +": no mask")
+    else:
+        print("test"+ i +": wearing mask")
